14034522558ad05b1e9acd75cbf2d28130a5bcad
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-08-08 16:25:38 -0500

    support jsx without import

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactRefresh from "@vitejs/plugin-react-refresh"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  esbuild: {
+    jsxInject: `import * as jsx from 'react'`,
+    jsxFactory: `jsx.createElement`,
+    jsxFragment: `jsx.Fragment`,
+  },
+  plugins: [reactRefresh()],
 })
