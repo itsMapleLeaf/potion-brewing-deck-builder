a1bdc286036cdd78a588db1925d5679b03e854cb
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-08-10 22:26:29 -0500

    starting with client side game

--- shared/helpers.ts
+++ shared/helpers.ts
@@ -3,3 +3,11 @@ import type { Falsy } from "./types"
 export function isTruthy<T>(value: T | Falsy): value is T {
   return !!value
 }
+
+export function pickRandom<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+export function randomRange(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,64 @@
-import { lazy, Suspense } from "react"
-import { HomePage } from "./HomePage"
-import { useRoute } from "./router"
+import produce from "immer"
+import { useState } from "react"
+import { randomRange } from "../shared/helpers"
 
-const GamePage = lazy(() =>
-  import("./GamePage").then((m) => ({ default: m.GamePage }))
-)
+type Piece = {
+  color: "green" | "orange" | "white"
+  value: number
+}
+
+const initialState = {
+  bag: [
+    { color: "green", value: 1 },
+    { color: "orange", value: 1 },
+    { color: "white", value: 1 },
+    { color: "white", value: 1 },
+    { color: "white", value: 1 },
+    { color: "white", value: 1 },
+    { color: "white", value: 2 },
+    { color: "white", value: 2 },
+    { color: "white", value: 3 },
+  ] as Piece[],
+  cauldron: [] as Piece[],
+}
 
 export function App() {
-  const route = useRoute()
+  const [state, setState] = useState(initialState)
+
+  function reset() {
+    setState(initialState)
+  }
+
+  function drawPiece() {
+    setState(
+      produce((draft) => {
+        const index = randomRange(0, draft.bag.length - 1)
+        const [piece] = draft.bag.splice(index, 1)
+        draft.cauldron.push(piece)
+      })
+    )
+  }
+
+  const cherryCount = state.cauldron
+    .filter(({ color }) => color === "white")
+    .reduce((count, { value }) => count + value, 0)
+
   return (
-    <Suspense fallback="Loading...">
-      {route.name === "home" && <HomePage />}
-      {route.name === "game" && <GamePage {...route.params} />}
-    </Suspense>
+    <main>
+      <button onClick={drawPiece}>Add ingredient</button>
+      <button onClick={reset}>Reset</button>
+      <h1>Cauldron</h1>
+      <p>cherry count: {cherryCount}</p>
+      <ol>
+        {state.cauldron.map(
+          (piece, index) =>
+            piece && (
+              <li key={index}>
+                {piece.color} {piece.value}
+              </li>
+            )
+        )}
+      </ol>
+    </main>
   )
 }
