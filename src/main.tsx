e3de12d5829a113b8a0e8d0c6a84aa561d502f22
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-08-11 01:10:01 -0500

    remove RouteProvider for now

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react"
 import { render } from "react-dom"
 import { App } from "./App"
-import { RouteProvider } from "./router"
 import "./style.css"
 
 render(
   <StrictMode>
-    <RouteProvider>
-      <App />
-    </RouteProvider>
+    <App />
   </StrictMode>,
   document.getElementById("root")
 )
